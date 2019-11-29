1fa372c475e520fe7d16c472f704e48c4ceca6c9
haditim <hadi@timachi.com>
2019-11-29 23:42:35 +0100

    first commit

---
+++ packages.el
@@ -0,0 +1,17 @@
+;; -*- no-byte-compile: t; -*-
+;;; .doom.d/packages.el
+
+;;; Examples:
+;; (package! some-package)
+;; (package! another-package :recipe (:host github :repo "username/repo"))
+;; (package! builtin-package :disable t)
+
+
+(package! elpy)
+(package! flycheck)
+(package! elpy)
+(package! py-autopep8)                     ;; Run autopep8 on save
+(package! blacken)
+(package! jedi)
+(package! yasnippet)
+(package! arduino-mode)
