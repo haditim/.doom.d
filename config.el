f6ef793e1857b5c0cc41e9a2f2e8436652fe61e1
Hadi Timachi <hadi@timachi.com>
2019-12-02 17:24:10 +0100

    Added google style python docstring

--- config.el
+++ config.el
@@ -41,3 +41,24 @@
   :desc "Docker-compose"  "c" #'docker-compose
   :desc "Docker"  "d" #'docker)
  )
+
+;; Custom yasnippets: https://emacs.stackexchange.com/questions/19422/library-for-automatically-inserting-python-docstring-in-google-style
+(defun python-args-to-google-docstring (text &optional make-fields)
+  "Return a reST docstring format for the python arguments in yas-text."
+  (let* ((indent (concat "\n" (make-string (current-column) 32)))
+         (args (python-split-args text))
+     (nr 0)
+         (formatted-args
+      (mapconcat
+       (lambda (x)
+         (concat "   " (nth 0 x)
+             (if make-fields (format " ${%d:arg%d}" (cl-incf nr) nr))
+             (if (nth 1 x) (concat " \(default " (nth 1 x) "\)"))))
+       args
+       indent)))
+    (unless (string= formatted-args "")
+      (concat
+       (mapconcat 'identity
+          (list "" "Args:" formatted-args)
+          indent)
+       "\n"))))
