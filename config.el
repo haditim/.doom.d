246ea158266b8c090f114372546077149d41f318
haditim <hadi@timachi.com>
2019-12-09 20:27:47 +0100

    added multiple-cursor keybinds

--- config.el
+++ config.el
@@ -95,3 +95,12 @@
 
 ;; bash for shell https://emacs.stackexchange.com/questions/28647/how-do-i-change-the-default-shell-for-shell-command
 (setq explicit-shell-file-name "/bin/bash")
+
+;; multiple cursor
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+
+(global-set-key (kbd "C-S-<mouse-1>") 'mc/add-cursor-on-click)
