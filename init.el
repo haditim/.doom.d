9577e46af935da8787c67624f02d6cb56201b010
Hadi Timachi <hadi@timachi.com>
2020-05-05 15:27:21 +0200

    Some small tweaks for formatting code

--- init.el
+++ init.el
@@ -92,7 +92,7 @@
        lsp
        ;;macos             ; MacOS-specific commands
        magit             ; a git porcelain for Emacs
-       ;;make              ; run make tasks from Emacs
+       make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
        pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
