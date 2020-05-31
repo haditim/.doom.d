8ef6536a622ae7a40e7e718b738da45d62a0fe74
Hadi Timachi <hadi@timachi.com>
2020-05-31 21:29:14 +0200

    solved the issue not opening help pop-up

--- init.el
+++ init.el
@@ -24,7 +24,7 @@
         +auto            ; as-you-type code completion
         +childframe)     ; a nicer company UI. Emacs +26 only!
        helm              ; the *other* search engine for love and life
-       ido               ; the other *other* search engine...
+       ;;ido               ; the other *other* search engine...
        ivy               ; a search engine for love and life
 
        :ui
